69fe78d82927d04d567079e34c92409d70263d74
Jan Feřtek <fertek@users.noreply.github.com>
2018-03-24 16:00:15 +0100

    Make fizzbuzz nicer (#3)

--- fb.py
+++ fb.py
@@ -1,8 +1,9 @@
 def fizzbuzz(number):
-    ret = ''
-    if number % 3 == 0:
-        ret += 'fizz'
-    if number % 5 == 0:
-        ret += 'buzz'
-    return ret or str(number)
- 
+    if number % 15 == 0:
+        return 'fizzbuzz'
+    elif number % 3 == 0:
+        return 'fizz'
+    elif number % 5 == 0:
+        return 'buzz'
+    else:
+        return str(number)
